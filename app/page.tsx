06c6744d16ae67d07c808ba95ec763f7fff55636
Kieran McKenna <160551320+kmck133@users.noreply.github.com>
2025-08-20 16:26:07 +1200

    Made header sticky and fixed layout and page.tsx (#50)

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ import Footer from '@/components/common/Footer'
 export default function Home() {
   return (
     <div className="home">
-      <Navbar />
       <div className="mt-64">
         <h1>New Arrivals</h1>
       </div>
       <PopCollections />
-      <Footer />
     </div>
   )
 }
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,8 +1,7 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
-import { MdAccountCircle, MdOutlineShoppingCart , MdClose } from "react-icons/md";
-import { MdOutlineSearch } from "react-icons/md";
+import { MdAccountCircle, MdOutlineShoppingCart , MdClose, MdOutlineSearch } from "react-icons/md";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
