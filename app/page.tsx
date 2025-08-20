558e0d4c75625d772e234a526d1fc70f71b5c5de
ntur101 <ntur101@aucklanduni.ac.nz>
2025-08-20 17:00:44 +1200

    added in the new arrivals component in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Navbar from '@/components/common/Navbar'
 import PopCollections from '@/components/home/PopCollections'
 import Footer from '@/components/common/Footer'
+import NewArrivals from '@/components/home/NewArrivals'
 
 export default function Home() {
   return (
     <div className="home">
       <Navbar />
-      <div className="mt-64">
-        <h1>New Arrivals</h1>
-      </div>
+      <NewArrivals />
       <PopCollections />
       <Footer />
     </div>
