cf549b5b3e70afc5486d1f97670b4a37fd0a1929
Meara Keelty <mkee115@aucklanduni.ac.nz>
2025-08-20 16:56:04 +1200

    Added not found and not implemented pages.

---
+++ app/not-implemented/page.tsx
@@ -0,0 +1,9 @@
+import NotImplementedPage from '@/components/not-implemented/notImplementedPage';
+
+export default function NotImplemented() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <NotImplementedPage />
+    </div>
+  )
+}
